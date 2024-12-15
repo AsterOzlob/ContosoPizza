6a621145a786d7dae716a3d3f6b5df733e224abb
AsterOzlob <tumoshenko204@mail.ru>
2024-12-15 21:13:00 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using ContosoPizza.Data;
+using ContosoPizza.Models;
+using ContosoPizza.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Настройка базы данных SQLite
+builder.Services.AddDbContext<PizzaContext>(options =>
+    options.UseSqlite("Data Source=pizza.db"));
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddScoped<PizzaService>();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapGet("/", async context =>
+{
+    context.Response.ContentType = "text/html";
+    await context.Response.SendFileAsync("wwwroot/html/index.html");
+});
+
+app.MapControllers();
+
+app.Run();
