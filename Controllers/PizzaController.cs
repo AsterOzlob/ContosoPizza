b6df49016dc7272cbf0a2eca6eee7e60e8301041
AsterOzlob <tumoshenko204@mail.ru>
2024-12-15 23:09:00 +0500

    Update PizzaController

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ContosoPizza.Models;
 using ContosoPizza.Services;
+using SQLitePCL;
 
 namespace ContosoPizza.Controllers
 {
@@ -9,5 +10,79 @@ namespace ContosoPizza.Controllers
     [ApiController]
     public class PizzaController : ControllerBase
     {
+        PizzaService _service;
+
+        public PizzaController(PizzaService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<Pizza> GetAll()
+        {
+            return _service.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> GetById(int id)
+        {
+            var pizza = _service.GetById(id);
+
+            if (pizza is null)
+            {
+                return NotFound();
+            }
+
+            return pizza;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Pizza newPizza)
+        {
+            var pizza = _service.Create(newPizza);
+            return CreatedAtAction(nameof(GetById), new { id = pizza!.Id }, pizza);
+        }
+
+        [HttpPost("{id}/addtopping")]
+        public IActionResult AddToping(int id, int toppingId)
+        {
+            var pizzaToUpdate = _service.GetById(id);
+
+            if(pizzaToUpdate is null)
+            {
+                return NotFound();
+            }
+
+            _service.AddTopping(id, toppingId);
+            return NoContent();
+        }
+
+        [HttpPost("{id}/updatesauce")]
+        public IActionResult UpdateSauce(int id, int sauceId)
+        {
+            var pizzaToUpdate = _service.GetById(id);
+
+            if (pizzaToUpdate is null)
+            {
+                return NotFound();
+            }
+
+            _service.updateSauce(id, sauceId);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var pizza = _service.GetById(id);
+
+            if (pizza is null)
+            {
+                return NotFound();
+            }
+
+            _service.DeleteById(id);
+            return Ok();
+        }
     }
 }
